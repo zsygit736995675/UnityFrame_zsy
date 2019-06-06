048a435a169680d55b9dfc2d73cf372131f86a8e
Zgit <736995675@qq.com>
2019-06-06 17:35:46 +0800

    pathopen

---
+++ UnityFrame_zsy/Frame_zsy/Assets/Editor/Tool/PathOpen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PathOpen  {
+    
+    [MenuItem("Tools/OpenDirectory/dataPath")]
+    static void OpendataPath()
+    {
+        System.Diagnostics.Process.Start(Application.dataPath);
+    }
+
+    [MenuItem("Tools/OpenDirectory/persistentDataPath")]
+    static void OpenpersistentDataPath()
+    {
+        System.Diagnostics.Process.Start(Application.persistentDataPath);
+    }
+
+    [MenuItem("Tools/OpenDirectory/streamingAssetsPath")]
+    static void OpenstreamingAssetsPath()
+    {
+        System.Diagnostics.Process.Start(Application.streamingAssetsPath);
+    }
+
+    [MenuItem("Tools/OpenDirectory/temporaryCachePath")]
+    static void OpentemporaryCachePath()
+    {
+        System.Diagnostics.Process.Start(Application.temporaryCachePath);
+    }
+
+}
